77637321bf0ad527b3f4f5ff8ddcafb177d53279
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2024-01-03 10:44:13 +0900

    [Add] SpriteFlip
    상하좌우 캐릭터 모션에 따라 캐릭터 스프라이트가 바뀌도록 설정

--- Assets/UJY_Player/Scripts/PlayerController.cs
+++ Assets/UJY_Player/Scripts/PlayerController.cs
@@ -6,11 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     //이동 이벤트 변수 선언
     public event Action<Vector2> OnMoveEvent;
-    public SpriteRenderer spriteRenderer;
+    private GameObject _mainSprite;
+    private GameObject _mainSpriteUp;
+    private GameObject _mainSpriteDown;
+    private SpriteRenderer _spriteRenderer;
+
+ 
     private Camera _camera;
 
     private void Awake()
     {
+        _mainSprite = transform.GetChild(0).gameObject;
+        _mainSpriteUp = transform.GetChild(1).gameObject;
+        _mainSpriteDown = transform.GetChild(2).gameObject;
+        _spriteRenderer = _mainSprite.GetComponent<SpriteRenderer>();
         _camera = Camera.main;
     }
 
@@ -28,35 +37,44 @@ public class PlayerController : MonoBehaviour
         Vector2 moveInput = value.Get<Vector2>().normalized;
         CallMoveEvent(moveInput);
 
-
+        
         //인풋시스템의 x값이 0일 때 y값에 따라 앞, 뒷모습으로 스프라이트 변경
         if (moveInput.x == 0)
         {
             if (moveInput.y > 0)
             {
+                _mainSprite.SetActive(false);
+                _mainSpriteDown.SetActive(false);
+                _mainSpriteUp.SetActive(true);
                 Debug.Log("위");
                 //뒷모습이 보이는 스프라이트로 변경
             }
 
             if (moveInput.y < 0)
             {
+                _mainSprite.SetActive(false);
+                _mainSpriteDown.SetActive(true);
+                _mainSpriteUp.SetActive(false);
                 Debug.Log("아래");
                 //앞모습이 보이는 스프라이트로 변경
             }
         }
         else
         {
-            //인풋시스템의 x값이 음수로 변하면 y축으로 뒤집고 양수로 바뀔 때 다시 원상태로 돌아가도록 하기
+            //좌, 우 모습으로 변경 후 인풋시스템의 x값이 음수로 변하면 y축으로 뒤집고 양수로 바뀔 때 다시 원상태로 돌아가도록 하기
+            _mainSpriteDown.SetActive(false);
+            _mainSpriteUp.SetActive(false);
+            _mainSprite.SetActive(true);
             if (moveInput.x > 0)
             {
                 Debug.Log("오른쪽");
-                spriteRenderer.flipY = false;
+                _spriteRenderer.flipX = false;
             }
 
             if (moveInput.x < 0)
             {
                 Debug.Log("왼쪽");
-                spriteRenderer.flipY = true;
+                _spriteRenderer.flipX = true;
             }
         }
 
