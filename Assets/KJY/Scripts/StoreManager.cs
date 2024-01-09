1fffe47b491fa661cc75988ebb77879f899754de
junyeong-0825 <scv9761@gmail.com>
2024-01-09 11:15:06 +0900

    [fix] ResourceShop
    머지하면서 메인씬에서 사라진 재료상점 복구

--- Assets/KJY/Scripts/StoreManager.cs
+++ Assets/KJY/Scripts/StoreManager.cs
@@ -8,6 +8,7 @@ public class StoreManager : MonoBehaviour
 {
     //소지금에 대한 정보를 받아와야함
     protected Inventory inventory;
+    public PlayerItemDataController playerItemDataController;
     public TMP_Text gold;
 
     public GameObject shopWindow;
@@ -22,7 +23,7 @@ public class StoreManager : MonoBehaviour
     {
         Instance = this;
         inventory = GameObject.Find("Player").GetComponent<Inventory>();
-        
+        playerItemDataController = GameObject.Find("Player").GetComponent<PlayerItemDataController>();
     }
     //public void Sell(ItemData item)
     //{
@@ -38,7 +39,20 @@ public class StoreManager : MonoBehaviour
         {
             
             Player.Instance.PlayerDataChange(1, -item.price);
-            inventory.AddItem(item);
+            //inventory.AddItem(item);
+            if (item.price == 200)
+            {
+                playerItemDataController.GetResource(Resources.RESOURCE1, 1);
+            }
+            else if(item.price == 500)
+            {
+                playerItemDataController.GetResource(Resources.RESOURCE2, 1);
+            }
+            else if(item.price == 800)
+            {
+                playerItemDataController.GetResource(Resources.RESOURCE3, 1);
+
+            }
         }
         
     }
